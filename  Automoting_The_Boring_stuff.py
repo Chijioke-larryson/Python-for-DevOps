2a7b109d89e43f512cf1d69a5e1009da2a53f5c6
Larryson Chijioke <larrysongift@gmail.com>
2026-04-04 11:48:25 +0100

    More lessons

---
+++  Automoting_The_Boring_stuff.py
@@ -0,0 +1,37 @@
+
+# name = input("What is your name ?: ")
+# print(f"Its nice to meet you,{name}")
+# print(f"The length of your name is {str(len(name))}")
+# age = float(input("How old are you ? : "))
+# age += 1
+# print(f"You will be {age} in a year")
+
+# Weight
+
+# weight = float(input("Enter your Weight: "))
+# unit = input("Enter Your Unit(L,K)")
+
+# if unit == "K":
+#     weight = weight * 2.205
+#     unit = "lbs."
+#     print(f"Your weight is: {round(weight, 2)}{unit}")
+
+# elif unit == "L":
+#     weight = weight / 2.205
+#     unit = 'Kg.'
+#     print(f"Your weight is: {round(weight, 2)}{unit}")
+
+
+# else:
+#     print(f" {unit} is Not valid")
+
+#print(help(str))
+
+# age = int(input("Enter your age: "))
+
+# while not age < 0:
+#     print("Age must be greater than '0' ")
+#     age = int(input("Enter your age: "))
+# print(f" Your age is: {age}")
+    
+")
---
+++ test.py
@@ -0,0 +1,17 @@
+# num = [1, 2, 3, 4, 6, 7, 8, 9, 10]
+
+# for x in num:
+#     if x % 2 == 1:
+#         print(x)
+
+
+# username = str(input("Enter your username: "))
+# username = username.lower()
+# while username != "admin":
+#     print("Invalid username. Please try again.")
+#     username = input("Enter your username: ")
+# print("Welcome, admin!")
+
+
+cars = ["Lexux", "BMW", "Mercedes", "Audi", "Porsche","Ferrari", "Lamborghini", "Maserati", "Bugatti", "Rolls-Royce"]
+print(cars[2::3])

dea02de74b64039c7469e2e04b9277283878cb3a
Larryson Chijioke <larrysongift@gmail.com>
2026-04-06 09:59:05 +0100

    lesson 4

--- test.py
+++ test.py
@@ -23,10 +23,43 @@
 # cars.sort()
 # print(cars)
 
-fruits = ["banana", "mango", "oranges", "carrot"]
-meats = ["chicken", "beef", "pork", "lamb"]
-vegatables = ["spinach", "kale", "broccoli", "cauliflower"]
+# fruits = ["banana", "mango", "oranges", "carrot"]
+# meats = ["chicken", "beef", "pork", "lamb"]
+# vegatables = ["spinach", "kale", "broccoli", "cauliflower"]
 
-grocries = [fruits, meats, vegatables]
-print(grocries[0][1])
+# grocries = [fruits, meats, vegatables]
+# print(grocries[0][1])
   
+# num_pad = ((1, 2, 3),
+#            (4, 5, 6),
+#            (7, 8, 9),
+#            ("*", 0, "#"))
+
+
+# for row in num_pad:
+#     for num in row:
+#         print(num, end="")
+#     print()
+
+# Dictionary
+
+capital = {"USA" : "Washington",
+              "UK" : "London",
+                "France" : "Paris",
+                "Germany" : "Berlin",
+                "Italy" : "Rome",
+                "Spain" : "Madrid",
+                "Russia" : "Moscow",
+           }
+# print(capital.get("Russia"))
+requsted_country = input("Enter a country: ")
+
+while requsted_country not in capital:
+    requsted_country = input("Enter a country: ")
+    if requsted_country in capital:
+        print(f"The capital of {requsted_country} is {capital[requsted_country]}")
+print(f"The capital of {requsted_country} is {capital[requsted_country]}")
+
+    
+
+
--- training.py
+++ training.py
@@ -1,43 +1,43 @@
-print("I Love Pizza")
+# print("I Love Pizza")
 
-#Strings
-last_name = "John"
-food = "rice"
-password = '123456789'
+# #Strings
+# last_name = "John"
+# food = "rice"
+# password = '123456789'
 
-print(f"Hello! {last_name},your favourite {food} is ready!")
+# print(f"Hello! {last_name},your favourite {food} is ready!")
 
-print(f" Your account password is {password}")
+# print(f" Your account password is {password}")
 
-#Integers
+# #Integers
 
-age = 30
-quantities = 100
-num_of_police = 100
+# age = 30
+# quantities = 100
+# num_of_police = 100
 
-print (f"You are {age} years old!")
-print (f"You bought {quantities} pack of biscuiut")
-print(f"There are {num_of_police} numbers of police")
+# print (f"You are {age} years old!")
+# print (f"You bought {quantities} pack of biscuiut")
+# print(f"There are {num_of_police} numbers of police")
 
-# Float
+# # Float
 
-cost = 99.99
-speed = 2.2
-gpa = 3.4
+# cost = 99.99
+# speed = 2.2
+# gpa = 3.4
 
-print(f"The shoe cost ${cost}")
-print(f"The jets top speed is mach {speed} ")
-print(f" Yor result is: {gpa} GPA")
+# print(f"The shoe cost ${cost}")
+# print(f"The jets top speed is mach {speed} ")
+# print(f" Yor result is: {gpa} GPA")
 
-# Boolean
-is_teacher = False
+# # Boolean
+# is_teacher = False
 
-if is_teacher :
-    print(f"You are a teacher, welcome")
-else:
-    print(f"You are not a teacher, Bye!!")
+# if is_teacher :
+#     print(f"You are a teacher, welcome")
+# else:
+#     print(f"You are not a teacher, Bye!!")
 
-    # # TypeCasting
+#     # # TypeCasting
 
     # name = "F-35A"
     # age = 6
@@ -63,9 +63,18 @@ else:
 
 # Area fo a rect cal
 
-length = float(input("Enter the length ?: "))
-width = float(input("Enter the width ?: "))
+# length = float(input("Enter the length ?: "))
+# width = float(input("Enter the width ?: "))
 
-area = length * width
+# area = length * width
 
-print(f"The area of this rectangle is: {area}cm²")
+# print(f"The area of this rectangle is: {area}cm²")
+
+numbers = [1, 2, 3, 4, 5, 6, 6, 7, 7, 7, 8, 9, 10]
+
+# for even_numbers in numbers:
+#     if even_numbers % 2 == 0 :
+#         print(even_numbers, end=" ")
+
+update = tuple(numbers)
+print(update)

c15ab4c87c57512669c8df13e2dc80739ab2ed07
Larryson Chijioke <larrysongift@gmail.com>
2026-04-08 12:20:04 +0100

    file handling

--- training.py
+++ training.py
@@ -76,5 +76,77 @@ numbers = [1, 2, 3, 4, 5, 6, 6, 7, 7, 7, 8, 9, 10]
 #     if even_numbers % 2 == 0 :
 #         print(even_numbers, end=" ")
 
-update = tuple(numbers)
-print(update)
+# update = tuple(numbers)
+# print(update)
+
+# functions
+# it is a block of reusable codew
+
+# song = False
+
+# while  song != False:
+#     print("Happy Birthday to You")
+
+# else:
+#     print("It's not your birthday")
+
+# def happy_birthday(name, age ):
+#     print(f"Happy Birthday to {name} ") 
+#     print(f"You are now  {age} years old" )
+#     print("Happy Birthday!!!")
+#     print()
+
+# happy_birthday("chijioke", 22)
+# happy_birthday("Jude", 100)
+# happy_birthday("Larryson", 101)
+
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello!! {username}, Here is your invoice 👇 ")
+#     print(f"Your total :${round(amount, 3)}  with a due date of{due_date}")
+
+# display_invoice("silentxbt", 10000000, "11-12-2001")
+
+
+#Return statement
+# def create_name(first, last):
+#     first = first.capitalize()
+#     last = last.capitalize()
+#     return first + " " + last
+
+# full_name = create_name("John", "Kenenchukwu")
+
+# print(full_name)
+
+# Module . its a python file that has coses you  want to include in your code 
+# Modeles are included using "import" or  you can create your own  module
+
+# print(help("list"))
+
+# Varaible scope is where a varaiable is visible and accessible 
+# Scope Resolution - (LEGB) Local, enclose , global built in
+
+#Exception handling is an event that interrupt the flow of a program (
+#  ZeroDivisionError, TpeError, ValueError)
+
+import json
+
+employee = {
+   "Mark" : "Jude",
+   "job" : "Engineer",
+   "Marital" : "Married",
+   "Kids":"two"
+}
+
+
+file_path = 'output.json'
+
+
+try:
+    with open(file_path, 'w') as file:
+         json.dump(employee, file, indent=4)
+         print(f"json file {'file_path'} was created")
+   
+    
+except FileExistsError: 
+    print("File already exists")
+    
